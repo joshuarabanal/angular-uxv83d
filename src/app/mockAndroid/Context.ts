defa4f6f8e00fdde351e4f55b599a40255801c62
joshuarabanal <joshuarabanal@gmail.com>
2020-06-07 15:56:53 -0400

    test

--- src/app/mockAndroid/Context.ts
+++ src/app/mockAndroid/Context.ts
@@ -11,7 +11,7 @@ export class Context {
   }
 
   public static async getString(name: string): Promise<string> {
-    if (Context.stringSpecific == null) {
+    if (Context.stringSpecific == null) {//n
       Context.stringSpecific = await (await fetch(
         "assets/res/values/" + Context.getLanguageCode() + ".json"
       )).json();
