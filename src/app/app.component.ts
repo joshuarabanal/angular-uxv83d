9ef8d4cb3cade47510bc025059295adec051435d
joshuarabanal <joshuarabanal@gmail.com>
2021-02-10 18:58:43 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  constructor() {
-
-  }
+export class AppComponent {
+  constructor() {}
 
   ngOnInit() {
     //
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
